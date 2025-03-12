d7662ea5f6e80b9d712dabcf0471423d32e5dfce
ankitsingh5423 <ankitkirola123455@gmail.com>
2025-03-12 23:43:49 +0530

    funcitonality

--- pomodoro-timer/index.js
+++ pomodoro-timer/index.js
@@ -1,5 +1,26 @@
 // store all the useful item in variable
 // start button per click karne pe timer start karna hai
 // stop per stop
-// reset per reset 
-// 0 anne per ek alert bhejna hai 
+// reset per reset
+// 0 anne per ek alert bhejna hai
+
+const btnStart = document.querySelectorAll(".btn-start");
+const btnStop = document.querySelectorAll(".btn-stop");
+const btnReset = document.querySelectorAll(".btn-reset");
+const timer = document.querySelector(".timer");
+const timerArray = timer.innerHTML.split(":");
+
+// console.log(timerArray);
+
+let sec = +timerArray[0] * 60 + +timerArray[1];
+// console.log(sec);
+
+let CountDown = () => {
+  counter = setInterval(() => {
+    --sec;
+    const minute = Math.floor(sec / 60);
+    const second = sec % 60;
+  }, 1000);
+};
+
+CountDown();
