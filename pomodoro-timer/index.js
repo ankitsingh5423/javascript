038adee8957d11f9dae8e50a179881cf7f551503
ankitsingh5423 <ankitkirola123455@gmail.com>
2025-03-13 00:49:50 +0530

    some changes

--- pomodoro-timer/index.js
+++ pomodoro-timer/index.js
@@ -4,23 +4,45 @@
 // reset per reset
 // 0 anne per ek alert bhejna hai
 
-const btnStart = document.querySelectorAll(".btn-start");
-const btnStop = document.querySelectorAll(".btn-stop");
-const btnReset = document.querySelectorAll(".btn-reset");
-const timer = document.querySelector(".timer");
-const timerArray = timer.innerHTML.split(":");
+const btnStart = document.querySelector(".btn-start");
+const btnStop = document.querySelector(".btn-stop");
+const btnReset = document.querySelector(".btn-reset");
+let timer = document.querySelector(".timer");
+const defaultMin = "25:00";
 
 // console.log(timerArray);
 
-let sec = +timerArray[0] * 60 + +timerArray[1];
 // console.log(sec);
+let counter;
+
+let isTimerStarted = false;
 
 let CountDown = () => {
-  counter = setInterval(() => {
-    --sec;
-    const minute = Math.floor(sec / 60);
-    const second = sec % 60;
-  }, 1000);
+  if (!isTimerStarted) {
+    let timerArray = timer.innerHTML.split(":");
+    let sec = +timerArray[0] * 60 + +timerArray[1];
+
+    counter = setInterval(() => {
+      --sec;
+      const minute = Math.floor(sec / 60);
+      const second = sec % 60;
+      // console.log(minute,second)
+      timer.innerHTML = `${minute}:${second}`;
+    }, 1000);
+    isTimerStarted = true;
+  }
 };
 
-CountDown();
+let stopCounter = () => {
+  if (isTimerStarted) {
+    clearInterval(counter);
+    isTimerStarted = false;
+  }
+};
+
+btnStart.addEventListener("click", CountDown);
+btnStop.addEventListener("click", stopCounter);
+btnReset.addEventListener("click", () => {
+  stopCounter();
+  timer.innerHTML = defaultMin;
+});
