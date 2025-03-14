547ac0de00de2268339d21d8653fe27f0016b59a
ankitsingh5423 <ankitkirola123455@gmail.com>
2025-03-14 20:46:53 +0530

    rock-paper-scissors-game project

---
+++ rock-paper-scissors-game/index.js
@@ -0,0 +1,6 @@
+// store all the usable items in varibale
+// make a function jisme ki jis bhi buttonp pe click ho raha hai uski value leke baki ke results get karlo
+
+const rock = document.querySelector(".rock") 
+const paper = document.querySelector(".paper") 
+const scissors = document.querySelector(".scissors"); 
