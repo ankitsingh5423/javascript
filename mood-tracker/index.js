2fcb80a330303be35bd893b5514029620c2cc62e
ankitsingh5423 <ankitkirola123455@gmail.com>
2025-03-18 01:56:40 +0530

    main funtionality compelete

--- mood-tracker/index.js
+++ mood-tracker/index.js
@@ -1,12 +1,50 @@
 let historyButton = document.querySelector(".history-button");
 let history = document.querySelector(".history");
-
-console.log("history-button :", historyButton);
+let currentEmoji = document.querySelector(".currentEmoji");
+let smile = document.querySelector(".smile");
+let sad = document.querySelector(".sad");
+let laugh = document.querySelector(".laugh");
+let smart = document.querySelector(".smart");
+let happySad = document.querySelector(".happySad");
 
 const fullHistory = () => {
   history.classList.toggle("show");
-  console.log(history)
+  console.log(history);
+  let oldHistory = JSON.parse(localStorage.getItem("moods")) || [];
+  history.innerHTML = oldHistory
+    .map((e) => {
+      return `<span>${e}</span>`;
+    })
+    .join("");
 };
 
+let currentFace = JSON.parse(localStorage.getItem("moods"));
+
+console.log(currentFace);
+currentEmoji.innerHTML =
+  currentFace == null || "" ? "" : currentFace[currentFace.length - 1];
+
+const updateMood = (e) => {
+  let arr = JSON.parse(localStorage.getItem("moods")) || [];
+  let selectedEmoji = e.target.innerText;
+  currentEmoji.innerText = selectedEmoji;
+  let currentEmojiValue = currentEmoji.innerText;
+
+  arr.push(currentEmojiValue);
+  localStorage.setItem("moods", JSON.stringify(arr));
+  console.log("arr:history", arr);
+
+  history.innerHTML = arr
+    .map((e) => {
+      return `<span>${e}</span>`;
+    })
+    .join("");
+};
+// let moodsArray = []
+
 historyButton.addEventListener("click", fullHistory);
-console.log(historyButton);
+smile.addEventListener("click", updateMood);
+sad.addEventListener("click", updateMood);
+smart.addEventListener("click", updateMood);
+laugh.addEventListener("click", updateMood);
+happySad.addEventListener("click", updateMood);
