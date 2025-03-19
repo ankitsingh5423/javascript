aeb6f88c22e92dc244e12cb0f2bf1aa89cceb8f2
ankitsingh5423 <ankitkirola123455@gmail.com>
2025-03-20 00:04:45 +0530

    compeleted project

--- quote-of-the-day/index.js
+++ quote-of-the-day/index.js
@@ -1,4 +1,5 @@
 const quote = document.querySelector(".quote");
+const quoteDisplay = document.querySelector(".quoteDisplay");
 const newQuoteBtn = document.querySelector(".newQuoteBtn");
 const copyBtn = document.querySelector(".copyBtn");
 const saveBtn = document.querySelector(".saveBtn");
@@ -23,19 +24,35 @@ const getQuote = async () => {
     console.error(error);
   }
 };
-if (quote.innerText == "" || null ? getQuote() : "");
+if (quote.innerText == "" || quote.innerText == null) {
+  getQuote();
+}
 
 const copyQuote = () => {
   navigator.clipboard.writeText(quote.innerText);
   toast = new Toast("Copied!", Toast.TYPE_DONE, 20000);
   //   console.log(quote)
 };
+
 configureToasts({
   topOrigin: -500, // [default=0] Y-axis origin of the messages (better to use CSS `transform` instead).
   rightOrigin: 500, // [default=0] Y-axis origin of the messages (better to use CSS `transform` instead).
 });
 
+const saveQuote = () => {
+  html2canvas(quoteDisplay).then((canvas) => {
+    const a = document.createElement("a");
+    a.href = canvas.toDataURL();
+    a.setAttribute("download", "quote");
+    a.style.display = "none";
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+  });
+};
+
 newQuoteBtn.addEventListener("click", getQuote);
 copyBtn.addEventListener("click", copyQuote);
+saveBtn.addEventListener("click", saveQuote);
 
 // console.log("fetch : ", getQuote());
